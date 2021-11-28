88b9cef60c8f746662b6ff8d0c8a65d03793fdf8
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-11-28 07:52:12 +0400

    feat: entities

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
