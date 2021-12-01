c04dd65228dfe04d36b574029ccf38ee6a4528e9
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-12-01 23:35:54 +0400

    feat: DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,19 +18,31 @@ namespace API.Controllers
             _context = context;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac=new HMACSHA512();
             var user= new AppUser
             {
-                UserName=username,
-                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName=registerDto.Username.ToLower(),
+                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt=hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private ActionResult<AppUser> BadRequest(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x=>x.UserName==username.ToLower());
         }
     }
 }
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
