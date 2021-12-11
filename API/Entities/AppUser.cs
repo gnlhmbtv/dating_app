7d78e5acc480d00e687b8dc339fcd2890b7b5f31
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-12-11 13:02:32 +0400

    feat:Extending the user entity(id,name,city,photo etc)

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,6 +11,17 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
         
     }
 }
