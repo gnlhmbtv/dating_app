699cb08a64b6433e732618e9ee8b4276a91f34f6
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-04 00:44:20 +0400

    feat: using automapper (photoUrl)

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -6,7 +6,8 @@ namespace API.DTOs
     public class MemberDto
     {
          public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } 
