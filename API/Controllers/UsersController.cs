6385a7a2392b6eee1434c3de7c79d7da3c241db6
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-05 01:22:26 +0400

    feat: configuring automapper (photourl is not null)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace API.Controllers
             _userRepository = userRepository;
         }
         [HttpGet]
-        //[AllowAnonymous]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetUsersAsync();
