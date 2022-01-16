7c259b50bf2af40daed1963e0cb5cc331e7ed3a8
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-16 11:18:50 +0400

    refactor: userdto, accountcontroller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
 
@@ -70,7 +71,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs 
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         private async Task<bool> UserExists(string username)
