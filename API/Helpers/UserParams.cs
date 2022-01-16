92a3a9ea2bd85fa403c294ce44002f9f3afb5656
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-16 11:13:38 +0400

    feat: adding additional filters (filtering with age)

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -13,5 +13,7 @@ namespace API.Helpers
         }
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 150;             
     }
 }
