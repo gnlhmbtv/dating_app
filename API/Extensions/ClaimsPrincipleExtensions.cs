60fcb76c75eb53e41536ef88a3c6db43a9f46a61
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-17 20:51:08 +0400

    feat: making the last active action filter more optimal

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -6,7 +6,11 @@ namespace API.Extensions
     {
         public static string GetUserName(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value); 
         }
     }
 }
