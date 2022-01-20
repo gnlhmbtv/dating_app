7c1080f82354003c05b0deca8e7783d459c5ef9b
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-20 14:13:53 +0400

    feat: adding the roles to the jwt token

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -29,6 +29,7 @@ namespace API.Controllers
             _mapper = mapper;
             _userRepository = userRepository;
         }
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
@@ -45,6 +46,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
+
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
