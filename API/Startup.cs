be7b708ac3bc191bfac6714fc6d2f639f178b76c
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-21 17:11:56 +0400

    feat: adding a fallback controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -70,6 +70,7 @@ namespace API
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
