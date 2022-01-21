38810567548a420f87664685116bbdb3d19879f4
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-21 11:47:56 +0400

    feat: implementing the unit of work

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -119,10 +119,5 @@ namespace API.Data
         {
             _context.Connections.Remove(connection);
         }
-
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
     }
 }
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -73,12 +73,7 @@ namespace API.Data
             .Include(p => p.Photos)
             .ToListAsync();
         }
-
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
+        
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
